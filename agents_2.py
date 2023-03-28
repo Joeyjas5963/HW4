5df01acad17fe368b006c0181561a64382abfe6b
becsedy <40574532+becsedy@users.noreply.github.com>
2023-03-28 18:12:50 -0400

    Add files via upload
    added unwilling and unpreferred agent

---
+++ agents_2.py
@@ -0,0 +1,30 @@
+import numpy as np
+from schedule import Schedule
+import random
+
+def max_pref(sol):
+
+    # sum each TAs preference values
+    pref_vec = np.sum(sol.pref_matx, axis=1)
+
+    # find TAs with lowest preference values
+    ta_idx_0, ta_idx_1 = np.argsort(pref_vec)[:2]
+
+    # isolate a random section that they were assigned to
+    ta_0, ta_1 = sol.sch_sol[ta_idx_0], sol.sch_sol[ta_idx_1]
+    sects_0, sects_1 = np.where(ta_0 == 1), np.where(ta_1 == 1)
+
+    sect_0 = random.choice(sects_0[0])
+    sect_1 = random.choice(sects_1[0])
+
+    # swap the two sections between the TAs
+    sol.sch_sol[ta_idx_0, sect_0] = 0
+    sol.sch_sol[ta_idx_0, sect_1] = 1
+    sol.sch_sol[ta_idx_1, sect_0] = 1
+    sol.sch_sol[ta_idx_1, sect_1] = 0
+
+    return sol
+
+Schedule.load_tas('tas.csv')
+Schedule.load_sections('sections.csv')
+sol = Schedule('test1.csv')
